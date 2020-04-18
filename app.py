1b1a3fd392983151c332cd9d7584935b55a19143
Moochul Kam <kamzzang1@naver.com>
2020-04-18 23:08:27 +0900

    First commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1.',
+        'author': 'Aaron'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2.'
+    }
+]
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
+
+@app.route('/home/users/<string:name>/posts/<int:id>')
+def hello(name, id):
+    return "Hello, " + name + ", your ID : " + str(id)
+
+@app.route('/onlyget', methods=['GET'])
+def get_req():
+    return 'You can only get this webpage.'
+
+if __name__ == "__main__":
+    app.run(debug=True)
